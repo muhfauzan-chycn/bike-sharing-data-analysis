90d1e52bd7624e08439e7403d418067bba6038f0
muhfouzan <ojanmohmadin1701@gmail.com>
2026-03-11 16:13:42 +0700

    Revise notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,52 +5,78 @@ import matplotlib.pyplot as plt
 
 st.title("Bike Sharing Data Dashboard")
 
-#load data
+# Load data
 df = pd.read_csv("dashboard/main_data.csv")
+
+
+# Filter Season
 
 season_option = st.selectbox(
     "Select Season",
     df["season"].unique()
 )
 
-
 filtered_df = df[df["season"] == season_option]
 
-# Weather vs Bike Rental
-st.subheader("Average Bike Rentals by Weather")
 
-weather_data = filtered_df.groupby("weathersit")["cnt"].mean()
+# 1. Weather vs Bike Rental
+
+st.subheader("Average Bike Rentals by Weather Condition")
+
+weather_data = filtered_df.groupby("weathersit")["cnt"].mean().reset_index()
 
 fig, ax = plt.subplots()
-sns.barplot(x=weather_data.index, y=weather_data.values, ax=ax)
+
+sns.barplot(
+    data=weather_data,
+    x="weathersit",
+    y="cnt",
+    ax=ax
+)
 
 ax.set_xlabel("Weather Condition")
-ax.set_ylabel("Average Rentals")
+ax.set_ylabel("Average Bike Rentals")
 
 st.pyplot(fig)
 
-# Season vs Bike Rental
-st.subheader("Average Bike Rentals by Season")
 
-season_data = df.groupby("season")["cnt"].mean()
+# 2. Peak Hour Rental
+
+st.subheader("Average Bike Rentals by Hour")
+
+hour_data = df.groupby("hr")["cnt"].mean().reset_index()
 
 fig, ax = plt.subplots()
-sns.barplot(x=weather_data.index, y=weather_data.values, ax=ax)
 
-ax.set_xlabel("Season")
-ax.set_ylabel("Average Rentals")
+sns.lineplot(
+    data=hour_data,
+    x="hr",
+    y="cnt",
+    marker="o",
+    ax=ax
+)
+
+ax.set_xlabel("Hour of the Day")
+ax.set_ylabel("Average Bike Rentals")
 
 st.pyplot(fig)
 
-# Casual vs Registered
-st.subheader("Average Bike Rentals by Type")
 
-user_data = df.groupby("casual")["registered"].mean()
+# 3. Casual vs Registered
+
+st.subheader("Total Rentals by User Type")
+
+user_data = df[["casual","registered"]].sum()
 
 fig, ax = plt.subplots()
-sns.barplot(x=weather_data.index, y=weather_data.values, ax=ax)
+
+sns.barplot(
+    x=user_data.index,
+    y=user_data.values,
+    ax=ax
+)
 
 ax.set_xlabel("User Type")
-ax.set_ylabel("Average Rentals")
+ax.set_ylabel("Total Rentals")
 
 st.pyplot(fig)
